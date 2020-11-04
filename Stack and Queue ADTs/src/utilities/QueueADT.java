a7e9c80b232e8e60c5c6cb0a6006d649ab84f7a4
Jaeyoung Kim <50346571+Jaeyoung-Kim-Dev@users.noreply.github.com>
2020-11-03 23:27:04 -0700

    Initialized the basic structure

---
+++ Stack and Queue ADTs/src/utilities/QueueADT.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package utilities;
+
+import java.io.Serializable;
+
+/**
+ * @author kornk
+ *
+ */
+public interface QueueADT extends Serializable {
+
+	
+	//push - Add an element
+	//pop - Remove and return an element
+	//peek - Get the first element without removing it
+	//isEmpty - Is there any elements?
+	//size - How many elements are there?
+}
