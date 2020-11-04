69b9d7e3bf7963bb73c14c9afb44245a993d6866
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-11-03 23:27:04 -0700

    Initialized the basic structure

---
+++ Stack and Queue ADTs/test/test/MyArrayListTests.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author kornk
+ *
+ */
+class MyArrayListTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * 	
+	 */
+	@Test
+	public void testAppending() {
+		//fail("Not yet implemented");
+	}
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testAdding() {
+		//fail("Not yet implemented");
+	}
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testReplacing() {
+		//fail("Not yet implemented");
+	}
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testClearingAll() {
+		//fail("Not yet implemented");
+	}
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testGetting() {
+		//fail("Not yet implemented");
+	}
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testFinding() {
+		//fail("Not yet implemented");
+	}
+	
+	
+
+}
