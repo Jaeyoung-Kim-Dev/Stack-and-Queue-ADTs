529daae0c45b810a0524ca9eb28af8ddb762f2a3
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-11-12 22:22:16 -0700

    Added classes

--- Stack and Queue ADTs/src/utilities/StackADT.java
+++ Stack and Queue ADTs/src/utilities/StackADT.java
@@ -5,17 +5,89 @@ package utilities;
 
 import java.io.Serializable;
 
+import exceptions.EmptyStackException;
+
 /**
- * @author kornk
+ * The stack data type operations for a stack.
+ * 
+ * @author Jaeyoung Kim
  *
  */
-public interface StackADT extends Serializable {
+public interface StackADT<E> extends Serializable {
+	/**
+	 * Pushes an element to the top of the stack.
+	 * 
+	 * Precondition:
+	 * 
+     * Postcondition:
+	 * 
+	 * @param e Element
+	 */
+	public void push(E e);
 
+	/**
+	 * Remove and returns the element at the top of the stack
+	 * 
+	 * Precondition:
+	 * 
+     * Postcondition:
+     * 
+	 * @return Element removed from Top of the stack.
+	 *
+	 * @throws EmptyStackException Thrown if stack has nothing to pop.
+	 */
+	public E Epop() throws EmptyStackException;
+
+	/**
+	 * Returns the element at the top of the stack without removing it from the stack.
+	 * 
+	 * Precondition:
+	 * 
+     * Postcondition:
+	 * 
+	 * @return Element at the top of the stack.
+	 * 
+	 * @throws EmptyStackException Thrown if stack has nothing to pop.
+	 */
+	public E peek( ) throws EmptyStackException;
+
+	/**
+	 * 
+	 * Precondition:
+	 * 
+     * Postcondition:
+     * 
+	 * Clears everything from the stack.
+	 */
+	public void clear();
+
+	/**
+	 * Checks if the stack is empty.
+	 * 
+	 * Precondition:
+	 * 
+     * Postcondition:
+     * 
+	 * @return True if stack has nothing in it.
+	 */
+	public boolean isEmpty();
 	
-	//push - Add an element
-	//pop - Remove and return an element
-	//peek - Get the first element without removing it
-	//isEmpty - Is there any elements?
-	//size - How many elements are there?
-		
+	
+	
+	
+	
+	
+	
+	/**
+	 * How many elements are there?
+	 * 
+	 * @param e Element
+	 */
+	public void size(E e);
+
+	// public void boolean equals( StackADT<E> that)
+
 }
+
+
+//E[] items = (E[])new Object[size];
--- Stack and Queue ADTs/test/test/MyArrayListTests.java
+++ Stack and Queue ADTs/test/test/MyArrayListTests.java
@@ -1,5 +1,27 @@
 /**
+ * ANNOTATIONS:
+ * @Test -  The annotation @Test identifies that a method is a test method. 
+ * @Ignore - Will ignore the test method. This is useful when the underlying code has been changed and 
+ * 		the test case has not yet been adapted. Or if the execution time of this test is too long to be included. 
+ * @Test(expected=Exception.class) - Fails, if the method does not throw the named exception. 
+ * @Test(timeout=100) - Fails, if the method takes longer than 100 milliseconds.
  * 
+ * ASSERTS: [] = optional message to display
+ * fail([String]) - Let the method fail. Might be used to check that a certain part of the code is not reached. 
+ * 		Or to have failing test before the test code is implemented.
+ * assertTrue(boolean) - Will always be true/false. Can be used to predefine a test result, 
+ * 		if the test is not yet implemented.
+ * assertTrue([String], boolean condition) - Checks that the boolean condition is true.
+ * assertsEquals([String], expected, actual) - Tests that two values are the same. 
+ * 		Note: for arrays the reference is checked not the content of the arrays.
+ * assertsEquals([String], expected, actual, tolerance) - Test that float or double values match. 
+ * 		The tolerance is the number of decimals which must be the same.
+ * assertNull([String], object) - Checks that the object is null.
+ * assertNotNull([String], object) - Checks that the object is not null.
+ * assertSame([String], expected, actual) - Checks that both variables refer to the same object.
+ * assertNotSame([String], expected, actual) - Checks that both variables refer to different objects. 
+ * 
+ * For a list of all test classes - check out http://junit.org/javadoc/latest/
  */
 package test;
 
@@ -9,11 +31,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import utilities.ListADT;
+
 /**
+ * Tests the MyArrayList.
+ * 
  * @author kornk
  *
  */
 class MyArrayListTests {
+	private ListADT<Integer> list;
 
 	/**
 	 * @throws java.lang.Exception
@@ -28,20 +55,52 @@ class MyArrayListTests {
 	@AfterEach
 	public void tearDown() throws Exception {
 	}
-
+		
 	/**
 	 * 	
 	 */
 	@Test
-	public void testAppending() {
-		//fail("Not yet implemented");
+	public void testAdd() {
+		int expected = 1;
+		
+		try {
+			this.list.add(1);
+			
+			assertFalse(this.list.isEmpty());
+			
+			int actual = this.list.get(0);
+			assertEquals(expected, actual);	
+		} catch (NullPointerException e) {
+			fail("");
+		} catch (IndexOutOfBoundsException e) {
+			fail("");
+		}
+		
 	}
 	
 	/**
 	 * 	
 	 */
 	@Test
-	public void testAdding() {
+	public void testAddInvalid() {
+		
+		assertTrue(this.list.isEmpty());
+		
+		try {
+			int actual = this.list.get(0);	
+			
+			fail("List is not empty");
+		} catch (IndexOutOfBoundsException e) {
+			
+		}
+	}
+	
+	
+	/**
+	 * 	
+	 */
+	@Test
+	public void testAppending() {
 		//fail("Not yet implemented");
 	}
 	
