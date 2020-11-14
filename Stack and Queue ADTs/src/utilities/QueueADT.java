8ca80521e7336547333c3b1ebf811e3e68b0d128
Jaeyoung Kim <50346571+Jaeyoung-Kim-Dev@users.noreply.github.com>
2020-11-14 00:05:51 -0700

    Documents done for ADTs

--- Stack and Queue ADTs/src/utilities/QueueADT.java
+++ Stack and Queue ADTs/src/utilities/QueueADT.java
@@ -6,120 +6,125 @@ package utilities;
 import java.io.Serializable;
 
 /**
+ * The queue data type operations for a queue.
+ * 
  * @author Jaeyoung Kim
- * @param <E>
- *
+ * 
  */
 public interface QueueADT<E> extends Serializable {
-
-	
 	/**
-	 * Mutator method to 'add to the tail of the queue
+	 * Mutator method to add to the tail of the queue.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists and an element is passed.
 	 * 
-	 * Postcondition:
+	 * Postcondition: Element is inserted to the tail of the queue.
 	 * 
-	 * @param o
+	 * @param e Element which is needed to be inserted.
 	 */
-	public void enqueue(E o);
+	public void enqueue(E e);
 	
 	/**
-	 * Mutator method to 'remove from the head of the queue
+	 * Mutator method to remove from the head of the queue.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It removes from the head of the queue and return the element.
 	 * 
-	 * @return
+	 * @return Element removed from the head of the queue.
 	 */
 	public E dequeue();
 	
 	/**
-	 * Accessor method to 'look at the element at the head of the queue
+	 * Accessor method to look at the element at the head of the queue.
 	 *
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It returns the element at the head of the queue.
 	 * 
-	 * @return
+	 * @return Element at the head of the queue.
 	 */
 	public E peek();
 	
 	/**
-	 * Accessor method to 'Checks if the queue is empty.
+	 * Accessor method to check if the queue is empty.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It returns true if the queue is empty otherwise it returns False.
 	 * 
-	 * @return
+	 * @return True if stack has nothing in it.
 	 */
 	public boolean isEmpty(); 
 	
 	/**
-	 * Accessor method to 'only if queue is of static capacity
+	 * Accessor method to only if queue is of static capacity.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It returns true if queue is of static capacity otherwise it returns False.
 	 * 
-	 * @return
+	 * @return True if queue is of static capacity.
 	 */
 	public boolean isFull();
 	
 	/**
-	 * Mutator method to 'clear all elements from the queue
+	 * Mutator method to clear all elements from the queue.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: Queue is empty.
 	 * 
 	 */
 	public void dequeueAll();
 	
 	/**
-	 * To be equal two stacks must contain equal items appearing in the same order.
+	 * Accessor method to check if two queues contain equal items appearing in the
+	 * same order.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists and a queue is passed.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It returns true if two queues contain equal items appearing in
+	 * the same order otherwise it returns False.
 	 * 
-	 * @param that
-	 * @return
+	 * @param that Queue which is needed to be compared.
+	 * @return True if two queues contain equal items appearing in the same order.
 	 */
 	public boolean equals(QueueADT<E> that);
 	
 	/**
+	 * Returns an array containing all of the items in this queue.
 	 *
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Precondition: A valid Queue object exists.
 	 * 
-	 * Postcondition:
+	 * Postcondition: It returns an array containing all of the items in this queue.
 	 * 
-	 * @return
+	 * @return Array containing all of the items in this queue.
 	 */
 	public Object[] toArray();
 	
 	/**
+	 * Returns an array containing all of the elements in this list in proper
+	 * sequence. The runtime type of the returned array is that of the specified
+	 * array.
 	 * 
+	 * Precondition: A valid Queue object exists and an array is passed.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Postcondition: It returns an array containing all of the elements in this
+	 * list in proper sequence
 	 * 
-	 * Postcondition:
-	 * 
-	 * @param copy
-	 * @return
+	 * @param copy Array into which the elements of the queue are to be stored.
+	 * @return array containing all of the elements in this list in proper sequence
 	 */
 	public E[] toArray(E[] copy);
 	
 	/**
+	 * Returns an iterator over the items contained in this stack.
 	 * 
+	 * Precondition: A valid Queue object exists and an element is passed.
 	 * 
-	 * Precondition: A valid Queue object exists and an integer value is passed.
+	 * Postcondition: It returns an iterator over the items contained in this stack.
 	 * 
-	 * Postcondition:
-	 * 
-	 * @return
+	 * @return Iterator over the items contained in this stack
 	 */
 	public Iterator<E> iterator();	
 }
--- Stack and Queue ADTs/src/utilities/StackADT.java
+++ Stack and Queue ADTs/src/utilities/StackADT.java
@@ -17,18 +17,18 @@ public interface StackADT<E> extends Serializable {
 	/**
 	 * Mutator method to push an element to the top of the stack.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists and an element is passed.
 	 * 
 	 * Postcondition: Element is inserted to the top of the stack.
 	 * 
-	 * @param e Element which is needed to be inserted
+	 * @param e Element which is needed to be inserted.
 	 */
 	public void push(E e);
 
 	/**
 	 * Mutator method to remove and returns the element at the top of the stack.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: Element at the top of the stack is removed and returned.
 	 * 
@@ -41,7 +41,7 @@ public interface StackADT<E> extends Serializable {
 	/**
 	 * Accessor method to return the topmost element in a stack without removing it.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: The topmost element is returned without removing it.
 	 * 
@@ -54,18 +54,17 @@ public interface StackADT<E> extends Serializable {
 	/**
 	 * Mutator method to make a stack empty.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: Stack is empty.
 	 * 
-	 * Clears everything from the stack.
 	 */
 	public void clear();
 
 	/**
 	 * Accessor method to check if the stack is empty.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: It returns true if stack is empty otherwise it returns False.
 	 * 
@@ -77,24 +76,24 @@ public interface StackADT<E> extends Serializable {
 	 * Accessor method to check if two stacks contain equal items appearing in the
 	 * same order.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists and a stack is passed.
 	 * 
 	 * Postcondition: It returns true if two stacks contain equal items appearing in
 	 * the same order otherwise it returns False.
 	 * 
-	 * @param that
-	 * @return
+	 * @param that Stack which is needed to be compared.
+	 * @return True if two stacks contain equal items appearing in the same order
 	 */
 	public boolean equals(StackADT<E> that);
 
 	/**
 	 * Returns an iterator over the items contained in this stack.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: It returns an iterator over the items contained in this stack.
 	 * 
-	 * @return
+	 * @return Iterator over the items contained in this stack
 	 */
 	public Iterator<E> iterator();
 
@@ -102,11 +101,11 @@ public interface StackADT<E> extends Serializable {
 	 * Returns an array containing all of the items in this stack. The top of the
 	 * stack corresponds to the first element of the array.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: It returns an array containing all of the items in this stack.
 	 * 
-	 * @return
+	 * @return Array containing all of the items in this stack
 	 */
 	public Object[] toArray();
 
@@ -115,13 +114,13 @@ public interface StackADT<E> extends Serializable {
 	 * sequence. The runtime type of the returned array is that of the specified
 	 * array.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists and an array is passed.
 	 * 
 	 * Postcondition: It returns an array containing all of the elements in this
 	 * list in proper sequence
 	 * 
-	 * @param copy
-	 * @return
+	 * @param copy Array into which the elements of the Stack are to be stored.
+	 * @return array containing all of the elements in this list in proper sequence
 	 */
 	public E[] toArray(E[] copy);
 
@@ -130,12 +129,12 @@ public interface StackADT<E> extends Serializable {
 	 * topmost Element being at position 1, and each Element deeper in the stack at
 	 * depth + 1.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists and an element is passed.
 	 * 
 	 * Postcondition: It returns the position of an Element on the stack.
 	 * 
-	 * @param e
-	 * @return
+	 * @param e Element to be searched.
+	 * @return the position of an Element on the stack
 	 */
 	public int search(E e);
 
@@ -143,7 +142,7 @@ public interface StackADT<E> extends Serializable {
 	 * Accessor method to check whether a specific element is present in the Stack
 	 * or not.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists and an element is passed.
 	 * 
 	 * Postcondition: It returns true if a specific element is present in the Stack
 	 * otherwise it returns False.
@@ -157,11 +156,11 @@ public interface StackADT<E> extends Serializable {
 	/**
 	 * Accessor method to return the number of Elements on the stack.
 	 * 
-	 * Precondition: A valid Stack object exists and an integer value is passed.
+	 * Precondition: A valid Stack object exists.
 	 * 
 	 * Postcondition: It returns the number of Elements on the stack.
 	 * 
-	 * @return
+	 * @return the number of Elements on the stack
 	 */
 	public int size();
 }
