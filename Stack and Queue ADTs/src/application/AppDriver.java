c7ddd68ba8f52672f5cd4ba9d78b916d683854f6
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-11-18 23:02:12 -0700

    Create AppDriver.java

---
+++ Stack and Queue ADTs/src/application/AppDriver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author kornk
+ *
+ */
+public class AppDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
