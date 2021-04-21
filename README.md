e966b992f7d804f6c7be08d68bb28fc408888544
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-04-20 21:10:41 -0600

    Moved files to the sub folder

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 <h1 align="center">Welcome to XML Parser 👋</h1>
 
-## What project it is
+## What project is it?
 
-> This application is parsing XML files with my own ADTs for a stack and a queue.
+> This is XML Parser, a project I did in the third semester of college (SAIT).
 
-## Structure of the program:
+## What is the purpose of the project?
+
+> This application is parsing XML files with my own Abstract Data Types (ADT) for a stack and a queue. There are utility classes used to implement the XML parser, which reads and confirms that the XML files are properly formatted.
+
+## What is XML?
+
+> Extensible Markup Language (XML) is a markup language that defines a set of rules for encoding documents in a format that is both human-readable and machine-readable.
+> <a href="https://en.wikipedia.org/wiki/XML" target=" _blank">Learn more on Wikipedia</a>
+
+## Structure of the program
 
 ![structur](https://github.com/Jaeyoung-Kim-Dev/Stack-and-Queue-ADTs/blob/main/img/ComponentsStructure.jpg?raw=true)
 
-## How to run the program:
+## How to run the program
 
-> 1.  Run Command Prompt (CMD)
+> 1.  Run Command Prompt (CMD) on Windows or Terminal on macOS or Linux
 > 2.  Change directory which includes the Parser.jar file.
 > 3.  Typle in the command lines with arguments like the example below.
-> 4.  Command examples below
+> 4.  Command examples:
 
 ```sh
 java -jar Parser.jar res/sample1.xml
@@ -22,6 +31,8 @@ java -jar Parser.jar res/sample1.xml
 ```sh
 java -jar Parser.jar res/sample2.xml
 ```
+
+## Testing
 
 ## Author
 
---
+++ Stack and Queue ADTs/test/test/MyStackTests.java
@@ -0,0 +1,211 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import exceptions.EmptyStackException;
+import utilities.MyArrayList;
+import utilities.MyStack;
+
+/**
+ * @author Jaeyoung Kim
+ *
+ */
+class MyStackTests {
+	private MyStack<String> myStackString;
+	private MyStack<Integer> myStackInt;
+
+	/**
+	 * @Before - Will execute the method before each test. This method can prepare
+	 *         the test environment (e.g. read input data, initialize the class).
+	 * 
+	 * @throws java.lang.Exception Exception
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+		myStackString = new MyStack<>();
+		myStackInt = new MyStack<>();
+	}
+
+	/**
+	 * @After - Will execute the method after each test. This method can cleanup the
+	 *        test environment (e.g. delete temporary data, restore defaults).
+	 * 
+	 * @throws java.lang.Exception Exception 
+	 */
+	@AfterEach
+	public void tearDown() throws Exception {
+		myStackString.clear();
+		myStackInt.clear();
+	}
+
+	@Test
+	void testPush() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		// Test the stack is not empty.
+		assertFalse(this.myStackString.isEmpty());
+
+		// Test the size is 4
+		assertEquals(4, myStackString.size());
+
+	}
+
+	@Test
+	void testPop() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		try {
+			/**
+			 * Stack should now be:
+			 * 
+			 * a -> b -> c -> d
+			 */
+
+			// Test the values as per index
+			assertEquals("d", myStackString.pop());
+			assertEquals("c", myStackString.pop());
+			assertEquals("b", myStackString.pop());
+			assertEquals("a", myStackString.pop());
+
+		} catch (EmptyStackException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	void testPeek() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		try {
+			/**
+			 * Stack should now be:
+			 * 
+			 * a -> b -> c -> d
+			 */
+			// Test the size is 4
+			assertEquals(4, myStackString.size());
+
+			assertEquals("d", myStackString.peek());
+
+			// Test the size is still 4
+			assertEquals(4, myStackString.size());
+
+		} catch (EmptyStackException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	void testClear() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		// Test the stack is not empty.
+		assertFalse(this.myStackString.isEmpty());
+
+		myStackString.clear();
+
+		// Test the stack is empty.
+		assertTrue(this.myStackString.isEmpty());
+	}
+	
+	@Test
+	void isEmpty() {
+		// Test the stack is empty.
+		assertTrue(this.myStackString.isEmpty());
+
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		// Test the stack is not empty.
+		assertFalse(this.myStackString.isEmpty());
+	}
+
+	@Test
+	void isEquals() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		MyStack<String> temp = new MyStack<>();
+		
+		temp.push("a");
+		temp.push("b");
+		temp.push("c");
+		temp.push("d");
+		
+		// Test the stack is not empty.
+		assertTrue(this.myStackString.equals(temp));
+	}
+	
+	@Test
+	void testToArray() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+		
+		Object[] temp = this.myStackString.toArray();
+		
+		// Test the index 2 is "c"
+		assertEquals("c", temp[2]);
+	}
+	
+	@Test
+	void testSearch() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+		
+		// Test the index of "c" is 3
+		assertEquals(2, myStackString.search("c"));
+	}
+	
+	@Test
+	void testContains() {
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+		
+		// Test the stack is not empty.
+		assertTrue(this.myStackString.contains("c"));
+	}
+	
+	@Test
+	void testSize() {
+		// Test the size is 0
+		assertEquals(0, myStackString.size());
+		
+		this.myStackString.push("a");
+		this.myStackString.push("b");
+		this.myStackString.push("c");
+		this.myStackString.push("d");
+
+		// Test the size is 4
+		assertEquals(4, myStackString.size());
+	}
+
+}
